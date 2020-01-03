87dd82dce099aa2c45858a86d7aef94db0fff4c6
Simon Højberg <r.hackr@gmail.com>
2020-01-03 11:11:36 -0500

    add ember-app back

---
+++ ember-app/app/app.js
@@ -0,0 +1,16 @@
+import Application from '@ember/application';
+import Resolver from './resolver';
+import loadInitializers from 'ember-load-initializers';
+import config from './config/environment';
+
+const App = Application.extend({
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
+  Resolver
+});
+
+loadInitializers(App, config.modulePrefix);
+
+window.EmberApp = App;
+
+export default App;
